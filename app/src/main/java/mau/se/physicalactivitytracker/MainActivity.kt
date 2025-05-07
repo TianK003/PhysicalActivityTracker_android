07e61b12e6f245648797d83e9b2c302d631a7594
TianK003 <tian.kljucanin@gmail.com>
2025-05-07 11:36:48 +0200

    Initial commit

---
+++ app/src/main/java/mau/se/physicalactivitytracker/MainActivity.kt
@@ -0,0 +1,28 @@
+package mau.se.physicalactivitytracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mau.se.physicalactivitytracker.ui.theme.PhysicalActivityTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PhysicalActivityTrackerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text("empty screen")
+                }
+            }
+        }
+    }
+}
