d204c0904c12ed4d71b5075bf59d64499fc5f6e0
TianK003 <tian.kljucanin@gmail.com>
2025-05-10 09:09:29 +0200

    Updated some of the libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,11 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.core)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.activity.ktx)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
